2722cc6f363667b4a21829c43360e8b5e38b036e
ib-ionut <birladionut@yahoo.it>
2025-05-21 16:09:14 +0200

    feat: added fade animation on pages navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,16 @@ import { ReactNode } from "react";
 import "@/styles/globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import PageTransition from "@/components/layout/PageTransition";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex min-h-screen flex-col font-sans">
         <Navbar />
-        <main className="flex-grow">{children}</main>
+        <PageTransition>
+          <main>{children}</main>
+        </PageTransition>
         <Footer />
       </body>
     </html>

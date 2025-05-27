727b4c9bf3d47dd16261c0f181737dadcef35eef
ib-ionut <birladionut@yahoo.it>
2025-05-27 10:34:52 +0200

    feat: added professional experience sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import LayoutContainer from "@/components/layout/LayoutContainer";
+
 import HeroSection from "@/components/home/HeroSection";
-import TrustedBySection from "@/components/home/TrustedBySection";
+import AboutCallout from "@/components/home/AboutCallout";
+import ProfessionalExperience from "@/components/home/ProfessionalExperience";
 import TechStatsSection from "@/components/home/TechStatsSection";
 import ProjectsPreview from "@/components/home/ProjectsPreview";
 import BlogTeaser from "@/components/home/BlogTeaser";
-import AboutCallout from "@/components/home/AboutCallout";
+import TrustedBySection from "@/components/home/TrustedBySection";
 import ContactCTA from "@/components/home/ContactCTA";
 
 export default function HomePage() {
@@ -12,6 +14,7 @@ export default function HomePage() {
     <LayoutContainer>
       <HeroSection />
       <AboutCallout />
+      <ProfessionalExperience />
       <TechStatsSection />
       <ProjectsPreview />
       <BlogTeaser />
--- src/components/layout/SectionContainer.tsx
+++ src/components/layout/SectionContainer.tsx
@@ -6,11 +6,18 @@ type Props = {
   children: ReactNode;
   fullWidth?: boolean;
   extraStyle?: string;
+  id?: string;
 };
 
-export default function SectionContainer({ children, fullWidth = false, extraStyle = "" }: Props) {
+export default function SectionContainer({
+  children,
+  fullWidth = false,
+  extraStyle = "",
+  id = "",
+}: Props) {
   return (
     <section
+      id={id}
       className={`${fullWidth ? "mb-40" : "mx-auto w-full max-w-7xl px-6 sm:px-6 lg:px-8 mb-40"} ${extraStyle}`}
     >
       {children}

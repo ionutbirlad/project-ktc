818ae8cd88384e1e63bc59b564f73291736a5faf
ib-ionut <birladionut@yahoo.it>
2025-05-29 19:27:11 +0200

    feat: added definitive spline scene

--- src/components/custom/ComingSoon.tsx
+++ src/components/custom/ComingSoon.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import Spline from "@splinetool/react-spline";
 
 export default function ComingSoon() {
-  return <Spline scene="/ComingSoon.splinecode" />;
+  return (
+    <section className="w-screen h-[calc(100vw*9/16)] max-h-screen overflow-hidden">
+      <Spline scene="/ComingSoon.splinecode" />
+    </section>
+  );
 }

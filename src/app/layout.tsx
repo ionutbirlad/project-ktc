14d3867dd7d854d569c7cb01b81ccc35d4f0361a
ib-ionut <birladionut@yahoo.it>
2025-05-29 12:14:54 +0200

    feat: created component for cooming soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,23 @@ import { ThemeProvider } from "@/styles/ThemeProvider";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 
+import ComingSoon from "@/components/custom/ComingSoon";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const MAINTENANCE_MODE = true;
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background text-foreground flex min-h-screen flex-col font-sans">
-        <ThemeProvider>
-          <Navbar />
-          <main className="flex-grow">{children}</main>
-          <Footer />
-        </ThemeProvider>
+        {MAINTENANCE_MODE ? (
+          <ComingSoon />
+        ) : (
+          <ThemeProvider>
+            <Navbar />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+          </ThemeProvider>
+        )}
       </body>
     </html>
   );

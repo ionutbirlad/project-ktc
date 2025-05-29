f4b967f8f00e32e52e9d424c54484f59378bc22d
ib-ionut <birladionut@yahoo.it>
2025-05-29 20:13:59 +0200

    fix: fixed some build issues

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,37 +1,41 @@
+// ✅ pagina in src/app/blog/[slug]/page.tsx
+
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { notFound } from "next/navigation";
+import { use } from "react";
 import MDXContent from "@/components/custom/MDXContent";
 
-export async function generateStaticParams() {
-  const dir = path.join(process.cwd(), "src/content/blog");
-  const files = fs.readdirSync(dir);
-  return files.map((file) => ({ slug: file.replace(/\.mdx?$/, "") }));
-}
+// ⚠️ workaround: marcare come statica o dinamica
+export const dynamic = "force-static";
 
-const getPost = async (slug: string) => {
+async function getPost(slug: string) {
   const filePath = path.join(process.cwd(), "src/content/blog", `${slug}.mdx`);
   if (!fs.existsSync(filePath)) return null;
 
   const source = fs.readFileSync(filePath, "utf8");
-  const file = matter(source);
-  const content = file.content;
-  const data = file.data as { title: string; date: string };
-
+  const { content, data } = matter(source);
   const mdxSource = await serialize(content);
-  return { mdxSource, meta: data };
-};
 
-type BlogPageProps = {
-  params: {
-    slug: string;
+  return {
+    mdxSource,
+    meta: data as { title: string; date: string },
   };
-};
+}
 
-export default async function BlogPage({ params }: BlogPageProps) {
-  const post = await getPost(params.slug);
+export function generateStaticParams(): { slug: string }[] {
+  const dir = path.join(process.cwd(), "src/content/blog");
+  const files = fs.readdirSync(dir);
+  return files.map((file) => ({
+    slug: file.replace(/\.mdx?$/, ""),
+  }));
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Page(props: any) {
+  const post = use(getPost(props?.params?.slug));
   if (!post) return notFound();
 
   return <MDXContent {...post} />;
--- src/components/custom/MDXContent.tsx
+++ src/components/custom/MDXContent.tsx
@@ -7,7 +7,7 @@ type Props = {
   meta: {
     title: string;
     date: string;
-    description: string;
+    description?: string;
   };
 };
 

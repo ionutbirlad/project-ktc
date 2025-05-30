fcac780ef9f22e27fcc496367abd2239d26b9bfd
ib-ionut <birladionut@yahoo.it>
2025-05-30 12:27:45 +0200

    fix: fixed scene on mobile devices

--- src/components/custom/ComingSoon.tsx
+++ src/components/custom/ComingSoon.tsx
@@ -4,8 +4,19 @@ import Spline from "@splinetool/react-spline";
 
 export default function ComingSoon() {
   return (
-    <section className="w-screen h-[calc(100vw*9/16)] max-h-screen overflow-hidden">
-      <Spline scene="/ComingSoon.splinecode" />
+    <section>
+      {/* Mobile */}
+      <div className="block md:hidden mobile-canvas-wrapper">
+        <Spline scene="/ComingSoonMobile.splinecode" />
+      </div>
+
+      {/* Desktop */}
+      <div className="hidden md:block w-full h-full fixed inset-0 flex items-center justify-center overflow-hidden bg-black">
+        <Spline
+          className="w-full h-full aspect-video object-cover"
+          scene="/ComingSoon.splinecode"
+        />
+      </div>
     </section>
   );
 }

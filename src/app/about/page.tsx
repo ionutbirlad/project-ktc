d0bd683b383421decd6f50c14f509ef8adb512ab
ionutbirlad <birladionut@yahoo.it>
2025-09-08 11:26:11 +0200

    chore: deleted old professional experience section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,10 @@
 import LayoutContainer from "@/components/layout/LayoutContainer";
 import AboutIntro from "@/components/about/AboutIntro";
-import ExperienceTimeline from "@/components/about/ExperienceTimeline";
 
 export default function AboutPage() {
   return (
     <LayoutContainer>
       <AboutIntro />
-      <ExperienceTimeline />
     </LayoutContainer>
   );
 }

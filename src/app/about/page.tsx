fadee469af8a636d5076a8a83262941d69606301
ionutbirlad <birladionut@yahoo.it>
2025-09-08 15:47:51 +0200

    feat: added `Testimonials` section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,7 @@ import LayoutContainer from "@/components/layout/LayoutContainer";
 import AboutIntro from "@/components/about/AboutIntro";
 import ProfessionalExperience from "@/components/about/ProfessionalExperience";
 import TrustedBySection from "@/components/about/TrustedBySection";
+import Testimonials from "@/components/about/Testimonials";
 
 export default function AboutPage() {
   return (
@@ -10,6 +11,7 @@ export default function AboutPage() {
       <AboutIntro extraStyle="bg-muted" />
       <ProfessionalExperience extraStyle="relative" fullWidth={true} />
       <TrustedBySection extraStyle="bg-muted" fullWidth={true} />
+      <Testimonials />
     </LayoutContainer>
   );
 }

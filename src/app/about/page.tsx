13798a625a59a961780c01115b51a80ab6731ba0
ionutbirlad <birladionut@yahoo.it>
2025-09-09 16:01:21 +0200

    feat: added `Divider` section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,13 @@ import ProfessionalExperience from "@/components/about/ProfessionalExperience";
 import TrustedBySection from "@/components/about/TrustedBySection";
 import Testimonials from "@/components/about/Testimonials";
 
+import SectionDivider from "@/components/general/SectionDivider";
+
 export default function AboutPage() {
   return (
     <LayoutContainer>
       <AboutIntro extraStyle="bg-muted p-0" fullWidth={true} />
+      <SectionDivider />
       <ProfessionalExperience extraStyle="relative" fullWidth={true} />
       <TrustedBySection extraStyle="bg-muted" fullWidth={true} />
       <Testimonials />
---
+++ src/components/general/SectionDivider.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type SectionDividerProps = {
+  extraStyle?: string;
+};
+
+export default function SectionDivider({ extraStyle }: SectionDividerProps) {
+  return (
+    <div className={`mx-auto bg-muted my-5 h-16 w-1 rounded-full sm:my-20 ${extraStyle}`}> </div>
+  );
+}

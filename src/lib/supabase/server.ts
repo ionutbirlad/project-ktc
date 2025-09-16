eab6239ed7a5db3b6e790f694201077445335535
ionutbirlad <birladionut@yahoo.it>
2025-09-16 17:28:23 +0200

    feat: added supabse client and server files + added middleware

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+
+export async function createClient() {
+  const cookieStore = await cookies(); // 👈 await needed in Next 15
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options, expires: new Date(0) });
+        },
+      },
+    }
+  );
+
+  return supabase;
+}

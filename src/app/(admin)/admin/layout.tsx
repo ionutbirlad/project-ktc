05b753138b13f5b10a243176d0f6c7ed6cc2304b
ionutbirlad <birladionut@yahoo.it>
2025-09-16 12:29:31 +0200

    feat: installed needed dependencies + removed unwanted elements in admin layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,13 +1,9 @@
 import { ReactNode } from "react";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Navbar />
       <main className="flex-grow">{children}</main>
-      <Footer />
     </div>
   );
 }

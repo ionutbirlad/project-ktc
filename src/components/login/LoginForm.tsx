67388530d40ce19719044a65c3051ef850001d22
ionutbirlad <birladionut@yahoo.it>
2025-10-01 10:52:15 +0200

    refactor: replaced `useFormStatus` with `useActionState` since depecated

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { signInWithEmailPassword } from "@/app/(auth)/actions";
 
 import { Button } from "@/components/ui/button";
@@ -19,7 +20,7 @@ function SubmitButton() {
 }
 
 export function LoginForm({ next }: { next: string }) {
-  const [state, formAction] = useFormState(signInWithEmailPassword, initialState);
+  const [state, formAction] = useActionState(signInWithEmailPassword, initialState);
 
   return (
     <form action={formAction} className="space-y-4">

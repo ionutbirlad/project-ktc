a70b9542e7059260e7520df8791e7c7e1c595e1b
ionutbirlad <birladionut@yahoo.it>
2025-10-02 16:47:39 +0200

    feat: added `SSR Auth` on admin layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,6 +1,20 @@
 import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({ children }: { children: ReactNode }) {
+import { createClient } from "@/lib/supabase/server";
+
+export default async function AdminLayout({ children }: { children: ReactNode }) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // If not logged → go to login (with redirect parameter to came back)
+  if (!user) {
+    redirect(`/login?redirect=${encodeURIComponent("/admin")}`);
+  }
+
   return (
     <div>
       <main className="flex-grow">{children}</main>

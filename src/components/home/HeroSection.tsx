f505a2eba3cadae817905683f01e3a07ce55d253
ionutbirlad <birladionut@yahoo.it>
2025-10-06 17:38:18 +0200

    feat: added basic structure to homepage hero

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,6 +4,9 @@ import SectionContainer from "@/components/layout/SectionContainer";
 import { Button } from "@/components/ui/button";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
+import Image from "next/image";
+import heroPhoto from "@/assets/images/home_hero-section.webp";
+
 type HeroSectionProps = {
   extraStyle?: string;
   fullWidth?: boolean;
@@ -13,12 +16,19 @@ export default function HeroSection({ extraStyle, fullWidth }: HeroSectionProps)
   return (
     <SectionContainer fullWidth={fullWidth} extraStyle={extraStyle}>
       <AspectRatio ratio={16 / 9} className="bg-muted">
-        <div className="h-full content-center py-16 text-center">
-          <h1 className="text-4xl font-bold mb-4">Kill The Competition</h1>
-          <p className="text-muted-foreground max-w-xl mx-auto mb-6">
-            Un progetto tecnico per distinguersi. Creato per sviluppatori, makers e visionari.
-          </p>
-          <Button>Scopri di più</Button>
+        <div className="max-w-screen-xl mx-auto flex h-full">
+          <div className="w-1/2 h-full bg-red-500">
+            <div className="h-full content-center py-16 text-center">
+              <h1 className="text-4xl font-bold mb-4">Kill The Competition</h1>
+              <p className="text-muted-foreground max-w-xl mx-auto mb-6">
+                Un progetto tecnico per distinguersi. Creato per sviluppatori, makers e visionari.
+              </p>
+              <Button>Scopri di più</Button>
+            </div>
+          </div>
+          <div className="w-1/2 h-full bg-orange-500 justify-center content-center">
+            <Image src={heroPhoto} alt="Foto di Ionut Birlad" className="object-contain" priority />
+          </div>
         </div>
       </AspectRatio>
     </SectionContainer>
